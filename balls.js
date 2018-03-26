5b40cb27cfdda41d493203d917a90567eb34d125
BjarkiThorJons <bjarkithorjonsson@gmail.com>
2018-03-26 18:42:02 +0000

    collision

--- balls.js
+++ balls.js
@@ -9,6 +9,7 @@ function random(min, max) {
   var num = Math.floor(Math.random() * (max - min + 1)) + min;
   return num;
 }
+// Loadar myndum
 let background = new Image();
 background.src = "myndir/background.jpg"
 
@@ -24,6 +25,9 @@ bomb.src = "myndir/bomb.gif"
 let boat = new Image();
 boat.src = "myndir/boat.png"
 
+let explosion = new Image();
+explosion.scr = "myndir/explosion.jpg"
+// Object constructor fyrir player
 function Player(x, y, size, size2, velX, velY){
   this.x = x;
   this.y = y;
@@ -32,22 +36,27 @@ function Player(x, y, size, size2, velX, velY){
   this.velX = velX;
   this.velY = velY;
 }
+// Prototype function fyrir player sem prentar myndina
 Player.prototype.draw = function(){
   ctx.drawImage(boat, this.x, this.y, this.size, this.size2);
 }
+// Prototype function til að hreyfa playerinn 
 Player.prototype.move = function(e){
     switch(e.keyCode) {
         case 37:
           player.x -= 10;
-          console.log(player.x)
           break;
         case 39:
           player.x += 10;
-          console.log(player.x)
           break;
     }   
   }
-function Ball(x, velX, velY, color, size, bounce) {
+Player.prototype.update = function(){
+  this.x += this.velX;
+  this.y += this.velY;
+}
+// Object constructor fyrir boltana
+function Ball(x,y, velX, velY, color, size, bounce) {
   this.x = x; 
   this.y = 130;
   this.velX = velX;
@@ -85,13 +94,14 @@ Ball.prototype.update = function() {
 Ball.prototype.collisionDetect = function() {
   for (var j = 0; j < balls.length; j++) {
     if (!(this === balls[j])) {
-      var dx = this.x - balls[j].x;
-      var dy = this.y - balls[j].y;
-      var distance = Math.sqrt(dx * dx + dy * dy);
-
-      if (distance < this.size + balls[j].size) {
-        balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
-       	this.bounce+=1
+      var dx = balls[j].x;
+      var dy = balls[j].y;
+      //var distance = Math.sqrt(dx * dx + dy * dy);
+      
+      if (dx >= player.x && dx - 20 <= player.x + player.size){
+          if (dy >= player.y && dy <= player.y + player.size2){
+            alert("You have been hit")
+        }
       }
     }
   }
@@ -105,7 +115,7 @@ player = new Player(
   )
 let balls = [];
 let x = 0
-let max = 0
+let max = 2
 let yes = 1
 let stig = 0
 function loop() {
@@ -117,7 +127,7 @@ function loop() {
   ctx.fillText(Math.round(stig),20,50);
   window.addEventListener("keydown", player.move, false);
   player.draw();
-  max += 0.005;
+  //max += 0.005;
   stig += 1;
   if (x >= width-100){
       yes = 0;
@@ -137,6 +147,7 @@ function loop() {
   while (balls.length < max) {
     var ball = new Ball(
       x,
+      130,
       random(-7,7),
       random(3,7),
       'rgb(255,0,0)',
@@ -149,6 +160,7 @@ function loop() {
   for (var i = 0; i < balls.length; i++) {
   	balls[i].draw();
 	  balls[i].update();
+    balls[i].collisionDetect();
     if (balls[i].bounce == 1){
     	balls.splice(i,1);
     }
