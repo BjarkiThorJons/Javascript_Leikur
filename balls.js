7f9663265e17809e6dd87063eeee0bac8d9ee72a
BjarkiThorJons <bjarkithorjonsson@gmail.com>
2018-03-30 12:11:16 +0000

    hljóð

--- balls.js
+++ balls.js
@@ -1,3 +1,5 @@
+//Bjarki Þór Jónsson
+// Styllingar fyrir canvas
 var canvas = document.querySelector('canvas');
 
 var ctx = canvas.getContext('2d');
@@ -25,29 +27,56 @@ bomb.src = "myndir/bomb.gif"
 let boat = new Image();
 boat.src = "myndir/boat.png"
 
+let boat2 = new Image();
+boat2.src = "myndir/boat2.png"
+
 let explosion = new Image();
 explosion.scr = "myndir/explosion.jpg"
+
+var song = document.createElement("audio");
+song.src = "audio/flight_of_the_bumblebee_2.mp3";
+song.loop = true;
+song.play();
+
+var explosionS = document.createElement("audio");
+explosionS.src = "audio/Explosion.mp3";
+
+
 // Object constructor fyrir player
-function Player(x, y, size, size2, velX, velY){
+function Player(x, y, size, size2, velX, velY, rotate){
   this.x = x;
   this.y = y;
   this.size = size;
   this.size2 = size2;
   this.velX = velX;
   this.velY = velY;
+  this.rotate = rotate
 }
 // Prototype function fyrir player sem prentar myndina
 Player.prototype.draw = function(){
-  ctx.drawImage(boat, this.x, this.y, this.size, this.size2);
+  if (this.rotate === 0){
+    ctx.drawImage(boat, this.x, this.y, this.size, this.size2);
+  }
+  else {
+    ctx.drawImage(boat2, this.x, this.y, this.size, this.size2);
+  }
+  if (this.hit === 1)
+    ctx.drawImage(explosion, this.x, this.y);
 }
 // Prototype function til að hreyfa playerinn 
 Player.prototype.move = function(e){
     switch(e.keyCode) {
         case 37:
-          player.x -= 10;
+          player.rotate = 1
+          if (player.x > 0){
+            player.x -= 30;
+          }
           break;
         case 39:
-          player.x += 10;
+          if (player.x + player.size < width){
+            player.x += 30;
+          }
+          player.rotate = 0 
           break;
     }   
   }
@@ -56,20 +85,20 @@ Player.prototype.update = function(){
   this.y += this.velY;
 }
 // Object constructor fyrir boltana
-function Ball(x,y, velX, velY, color, size, bounce) {
+function Ball(x, y, velX, velY, color, size, bounce) {
   this.x = x; 
-  this.y = 130;
+  this.y = y;
   this.velX = velX;
   this.velY = velY;
   this.color = color;
   this.size = size;
   this.bounce = bounce
 }
-
+// Prototype function fyrir Ball sem prentar myndina
 Ball.prototype.draw = function() {
   ctx.drawImage(bomb, this.x, this.y, this.size, this.size);
 }
-
+// Prototype function fyrir Ball sem fynnur út hvort boltinn snertir endana á skjánumn
 Ball.prototype.update = function() {
   if ((this.x + this.size) >= width) {
     this.bounce = 1;
@@ -98,9 +127,14 @@ Ball.prototype.collisionDetect = function() {
       var dy = balls[j].y;
       //var distance = Math.sqrt(dx * dx + dy * dy);
       
-      if (dx >= player.x && dx - 20 <= player.x + player.size){
+      if (dx >= player.x && dx <= player.x + player.size){
           if (dy >= player.y && dy <= player.y + player.size2){
-            alert("You have been hit")
+            max = 0
+            stig = 0
+            player.x = 100
+            x = 100
+            //song.currentTime = 0
+            explosionS.play();
         }
       }
     }
@@ -115,19 +149,21 @@ player = new Player(
   )
 let balls = [];
 let x = 0
-let max = 2
+let max = 0
 let yes = 1
 let stig = 0
 function loop() {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.50)';
-  //ctx.fillRect(0, 0, width, height);
   ctx.drawImage(background, 0, 0, width, height);
   ctx.fillStyle = "white";
   ctx.font = "60px Old English Text MT";
   ctx.fillText(Math.round(stig),20,50);
   window.addEventListener("keydown", player.move, false);
   player.draw();
-  //max += 0.005;
+  if (max <= 10){
+    max += 0.005;
+  };
+  console.log(max)
   stig += 1;
   if (x >= width-100){
       yes = 0;
@@ -138,7 +174,6 @@ function loop() {
     if (yes == 1){
       x += 6;
       ctx.drawImage(bomber2, x-260, 0, 446, 150);
-
     }
     else{
       x -=6
@@ -151,12 +186,11 @@ function loop() {
       random(-7,7),
       random(3,7),
       'rgb(255,0,0)',
-      //'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
       40
     );
     balls.push(ball);
   };
-  
+
   for (var i = 0; i < balls.length; i++) {
   	balls[i].draw();
 	  balls[i].update();
